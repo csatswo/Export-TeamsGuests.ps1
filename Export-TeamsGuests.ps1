96f2d0fc4446954d6e0cf38e8a538e419c8a4502
csatswo <68028099+csatswo@users.noreply.github.com>
2021-01-09 16:28:00 -0800

    Overhaul of script
    Script now creates a custom object to fix issues with CSV export.

--- Export-TeamsGuests.ps1
+++ Export-TeamsGuests.ps1
@@ -2,13 +2,13 @@
 
 .SYNOPSIS
  
-    Get-TeamsGuests.ps1 - Creates a CSV on the desktop of all Teams guests
+    Export-TeamsGuests.ps1 - Displays a list and exports a CSV of Teams guests
  
 .DESCRIPTION
 
     Author: csatswo
 
-    This script will output a list of the Teams guests in the terminal and will save a CSV on the desktop.
+    This script will output a list of the Teams guests in the terminal and will save a CSV of the results.
     
 .LINK
 
@@ -16,19 +16,24 @@
  
 .EXAMPLE 
     
-    .\Get-TeamsGuests.ps1 -username admin@domain.com -path C:\Temp\guests.csv
+    .\Export-TeamsGuests.ps1 -Username admin@domain.onmicrosoft.com -Path C:\Temp\guests.csv    
+
+        Displays a formatted table of Teams with guests and the guests of the Team.
+
+        TeamDisplayName GuestName      GuestUser                                                     
+        --------------- ---------      ---------                                                     
+        Contoso         Tony Stark     tony.stark_domain.com#EXT#@domain.onmicrosoft.com
+        Contoso         Bruce Banner   bruce.banner_domain.com#EXT#@domain.onmicrosoft.com              
+        Test Team 1     csatswo        csatswo_domain.com#EXT#@domain.onmicrosoft.com
 
 #>
 
 # Script setup
 
 Param(
-    [Parameter(mandatory=$true)][String]$username,
-    [Parameter(mandatory=$true)][String]$path
+    [Parameter(mandatory=$True)][String]$Username,
+    [Parameter(mandatory=$True)][String]$Path
 )
-
-$teams = @()
-$guests = @()
 
 # Check for Teams module and install if missing
 
@@ -48,19 +53,57 @@ if (Get-Module -ListAvailable -Name MicrosoftTeams) {
 # Connect to MS Teams
 
 Import-Module MicrosoftTeams
-Connect-MicrosoftTeams -AccountId $username
-Add-Content -Path $path -Value "TeamName,GuestUser"
-$teams = Get-Team
-Foreach ($team in $teams) {
-    $teamid = $team.GroupId
-    $teamname = $team.DisplayName
-    $guests = Get-TeamUser -GroupId $teamid -Role Guest
-    Foreach ($guest in $guests) {
-        $guestname = $guest.User
-        Write-Host `n
-        Write-Host ($teamname,$guestname) -Separator " --> " -ForegroundColor Yellow
-        Add-Content -Path $path -Value "$teamname,$guestname"
+Connect-MicrosoftTeams -AccountId $Username
+
+# Start script loops
+
+$Teams = @()
+$Teams = Get-Team
+$CustomObject = @()
+
+Foreach ($Team in $Teams) {
+    
+    $TeamId = @()
+    $TeamId = $Team.GroupId
+    
+    $Guests = @()
+    $Guests = Get-TeamUser -GroupId $TeamId -Role Guest
+
+    Foreach ($Guest in $Guests) {
+        
+        $CustomProperties = @{
+            GuestUserId = $Guest.UserId
+            GuestUser = $Guest.User
+            GuestName = $Guest.Name
+            GuestRole = $Guest.Role
+            TeamGroupId = $Team.GroupId
+            TeamDisplayName = $Team.DisplayName
+            TeamVisibility = $Team.Visibility
+            TeamArchived = $Team.Archived
+            TeamMailNickName = $Team.MailNickName
+            TeamDescription = $Team.Description
+            }
+
+        $ObjectProperties = New-Object -TypeName PSObject -Property $CustomProperties
+        $CustomObject += $ObjectProperties
+
         }
+
     }
 
-Write-Host ""`nReport saved at" $path" -ForegroundColor Cyan
+Write-Host "`n`nThe following guests were found:" -ForegroundColor Cyan
+Write-Output $CustomObject | Select-Object TeamDisplayName,GuestName,GuestUser | Sort-Object -Property TeamDisplayName | Format-Table
+
+$CustomObject | Export-Csv -Path $Path -NoTypeInformation
+
+Write-Host "Export saved to " -ForegroundColor Cyan -NoNewline
+Write-Host $Path -ForegroundColor Yellow -NoNewline
+Write-Host `n
+
+<#
+This section is experimental and is commented out intentionally.
+
+function Get-TeamsGuests {
+    Write-Output $CustomObject | Select-Object TeamDisplayName,TeamGroupId,GuestName,GuestUser | Sort-Object -Property TeamDisplayName
+    }
+#>
